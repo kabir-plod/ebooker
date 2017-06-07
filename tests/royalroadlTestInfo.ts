d74691e0ec4d68a30577816ac4c9f50b45b96ce8
kabir-plod <kabirk@live.com>
2017-06-07 21:57:34 +0800

    pass royalroadltests

--- tests/royalroadlTestInfo.ts
+++ tests/royalroadlTestInfo.ts
@@ -9,16 +9,16 @@ const royalroadlTestInfo: TestInfo[] =
 		title: 'Trapped: The GM',
 		author: 'Troll',
 		chapterUrls: [
-			'https://royalroadl.com/fiction/chapter/38623',
-			'https://royalroadl.com/fiction/chapter/38944',
-			'https://royalroadl.com/fiction/chapter/39646',
-			'https://royalroadl.com/fiction/chapter/39665',
-			'https://royalroadl.com/fiction/chapter/39940',
-			'https://royalroadl.com/fiction/chapter/40419',
-			'https://royalroadl.com/fiction/chapter/42075',
-			'https://royalroadl.com/fiction/chapter/42119',
-			'https://royalroadl.com/fiction/chapter/42362',
-			'https://royalroadl.com/fiction/chapter/42372'
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/38623/1-people-who-just-won-t-shut-up',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/38944/2-playing-player',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/39646/3-a-holy-item',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/39665/4-the-taste-of-despair',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/39940/5-the-master-of-excuses',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/40419/6-manmade-monster',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/42075/7-jumping-to-conclusions-can-be-a-danger',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/42119/8-you-can-t-be-my-cat',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/42362/9-when-your-enemies-band-together',
+			'https://royalroadl.com/fiction/3984/trapped-the-gm/chapter/42372/10-showdown-above-the-heavens'
 		],
 		parser: RoyalroadlParser.getParserReturner()
 	}
